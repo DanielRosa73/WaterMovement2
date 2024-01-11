73832aa9b94d398ccd6347bbb2da3904712fba5e
Justin Jaecker <justin.jaecker@epita.fr>
2024-01-11 15:52:06 +0100

    render face

--- src/main.cpp
+++ src/main.cpp
@@ -2,9 +2,27 @@
 #include <GLFW/glfw3.h>
 
 #include <iostream>
+#include <vector>
+
+#include <graphics/shader.h>
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void processInput(GLFWwindow* window);
+
+// Fonction pour générer les sommets de la grille
+std::vector<float> generateGridVertices(int gridSize) {
+    std::vector<float> vertices;
+    for (int i = -gridSize; i <= gridSize; i++) {
+        // Ligne verticale
+        vertices.push_back((float)i); vertices.push_back(0.0f); vertices.push_back((float)-gridSize);
+        vertices.push_back((float)i); vertices.push_back(0.0f); vertices.push_back((float)gridSize);
+
+        // Ligne horizontale
+        vertices.push_back((float)-gridSize); vertices.push_back(0.0f); vertices.push_back((float)i);
+        vertices.push_back((float)gridSize); vertices.push_back(0.0f); vertices.push_back((float)i);
+    }
+    return vertices;
+}
 
 int main() {
     // Initialisation de GLFW
@@ -38,15 +56,51 @@ int main() {
     // Enregistrer la fonction de rappel pour le redimensionnement de la fenêtre
     glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
+    GLfloat vertices[] {
+        -0.5f, -0.5f, 0.0f,
+        -0.5f,  0.5f, 0.0f,
+         0.5f,  0.5f, 0.0f,
+         0.5f, -0.5f, 0.0f
+    };
+
+    GLuint indices[] {
+        0, 1, 2,
+        2, 0, 3
+    };
+
+    GLuint VAO, VBO, EBO;
+    glGenVertexArrays(1, &VAO);
+    glGenBuffers(1, &VBO);
+    glGenBuffers(1, &EBO);
+
+    glBindVertexArray(VAO);
+
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (void*)0);
+    glEnableVertexAttribArray(0);
+
+    Shader shaderProgram("C:\\Users\\justi\\Desktop\\OGLA\\POGLA\\shaders\\grid.vtx", "C:\\Users\\justi\\Desktop\\OGLA\\POGLA\\shaders\\grid.frg");
+
     // Boucle de rendu
     while (!glfwWindowShouldClose(window)) {
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
         // Entrée
         processInput(window);
 
         // Rendu
         // (Ici, vous pouvez ajouter le code de rendu)
-
+        shaderProgram.use();
+        glBindVertexArray(VAO);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+        glBindVertexArray(0);
         // Échange des tampons et sondage des événements d'IO
+
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
