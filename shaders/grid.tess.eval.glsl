72dc606191c56aad1413a835797ea889b67feb69
daniel rosa <daniel.rosa@epita.fr>
2024-01-17 11:19:10 +0100

    added bilinear interpolation

--- shaders/grid.tess.eval.glsl
+++ shaders/grid.tess.eval.glsl
@@ -21,6 +21,25 @@ float getHeight(vec2 normalizedPos) {
     return texture(waterHeightMap, normalizedPos).r;
 }
 
+float bilinearInterpolate(float bottomLeft, float topLeft, float bottomRight, float topRight, vec2 fraction) {
+    float bottom = mix(bottomLeft, bottomRight, fraction.x);
+    float top = mix(topLeft, topRight, fraction.x);
+    return mix(bottom, top, fraction.y);
+}
+
+float interpolatedHeight(vec2 normalizedPos) {
+    vec2 gridPos = normalizedPos * float(WATERSIZE);
+    vec2 floorPos = floor(gridPos);
+    vec2 fraction = gridPos - floorPos;
+
+    float bottomLeft = texture(waterHeightMap, floorPos / float(WATERSIZE)).r;
+    float topLeft = texture(waterHeightMap, (floorPos + vec2(0.0, 1.0)) / float(WATERSIZE)).r;
+    float bottomRight = texture(waterHeightMap, (floorPos + vec2(1.0, 0.0)) / float(WATERSIZE)).r;
+    float topRight = texture(waterHeightMap, (floorPos + vec2(1.0, 1.0)) / float(WATERSIZE)).r;
+
+    return bilinearInterpolate(bottomLeft, topLeft, bottomRight, topRight, fraction);
+}
+
 vec2 getHorizontalDisplacement(vec2 normalizedPos) {
     float displacement = sin(deltaTime + length(normalizedPos) * 10.0) * 0.01;
     return normalizedPos + vec2(displacement);
@@ -29,14 +48,14 @@ vec2 getHorizontalDisplacement(vec2 normalizedPos) {
 void main() {
     vec3 worldPos = getWorldPos(gl_TessCoord);
     vec2 normalizedPos = (worldPos.xz + vec2(0.5 * WATERSIZE)) / WATERSIZE;
-    float height = getHeight(normalizedPos);
+    float height = interpolatedHeight(normalizedPos);
     worldPos.y += height;
 
     // Calculate normals (no dfdy or dfdy)
-    float heightLeft = getHeight(normalizedPos + vec2(-1.0 / WATERSIZE, 0.0));
-    float heightRight = getHeight(normalizedPos + vec2(1.0 / WATERSIZE, 0.0));
-    float heightDown = getHeight(normalizedPos + vec2(0.0, -1.0 / WATERSIZE));
-    float heightUp = getHeight(normalizedPos + vec2(0.0, 1.0 / WATERSIZE));
+    float heightLeft = interpolatedHeight(normalizedPos + vec2(-1.0 / WATERSIZE, 0.0));
+    float heightRight = interpolatedHeight(normalizedPos + vec2(1.0 / WATERSIZE, 0.0));
+    float heightDown = interpolatedHeight(normalizedPos + vec2(0.0, -1.0 / WATERSIZE));
+    float heightUp = interpolatedHeight(normalizedPos + vec2(0.0, 1.0 / WATERSIZE));
 
     vec3 tangent = vec3(2.0 / WATERSIZE, heightRight - heightLeft, 0.0);
     vec3 bitangent = vec3(0.0, heightUp - heightDown, 2.0 / WATERSIZE);
